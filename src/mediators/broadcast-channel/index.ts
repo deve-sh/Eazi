de181c6622873b80e5c7682048d8c66bc024af1e
Devesh Kumar <devesh2027@gmail.com>
2023-08-22 19:24:56 +0530

    feat: Listeners map

--- src/mediators/broadcast-channel/index.ts
+++ src/mediators/broadcast-channel/index.ts
@@ -1,32 +1,44 @@
 // Broadcast Channel based Event Bridge
 
 import { v4 as uuid } from "uuid";
-import type { CommunicationManager, Listener } from "../interface";
+import type {
+	CommunicationManager,
+	Listener,
+	NativeListener,
+} from "../interface";
 
 class BroadcastChannelBasedMediator implements CommunicationManager {
-  broadcastChannelId: string;
-  broadcastChannel: BroadcastChannel;
+	broadcastChannelId: string;
+	broadcastChannel: BroadcastChannel;
+	listeners: Map<Listener, NativeListener> = new Map();
 
-  constructor(name: string) {
-    this.broadcastChannelId = name || uuid();
-    this.broadcastChannel = new BroadcastChannel(this.broadcastChannelId);
-  }
+	constructor(name: string) {
+		this.broadcastChannelId = name || uuid();
+		this.broadcastChannel = new BroadcastChannel(this.broadcastChannelId);
+	}
 
-  sendMessage(message: unknown) {
-    this.broadcastChannel.postMessage(message);
-  }
+	sendMessage(message: unknown) {
+		this.broadcastChannel.postMessage(message);
+	}
 
-  onMessage(listener: Listener) {
-    this.broadcastChannel.addEventListener("message", listener);
-  }
+	onMessage(listener: Listener) {
+		const associatedListener = (event) => {
+			listener(event.data, event);
+		};
+		this.listeners.set(listener, associatedListener);
+		this.broadcastChannel.addEventListener("message", associatedListener);
+	}
 
-  removeListener(listener: Listener) {
-    this.broadcastChannel.removeEventListener("message", listener);
-  }
+	removeListener(listener: Listener) {
+		this.broadcastChannel.removeEventListener(
+			"message",
+			this.listeners.get(listener) as NativeListener
+		);
+	}
 
-  close() {
-    this.broadcastChannel.close();
-  }
+	close() {
+		this.broadcastChannel.close();
+	}
 }
 
 export default BroadcastChannelBasedMediator;
--- src/mediators/interface/index.ts
+++ src/mediators/interface/index.ts
@@ -1,8 +1,13 @@
-export type Listener = (event: MessageEvent) => void;
+export type NativeListener = (event: MessageEvent) => void;
+
+export type Listener = (
+	eventData?: MessageEvent["data"],
+	event?: MessageEvent
+) => void;
 
 export interface CommunicationManager {
-  onMessage: (listener: Listener) => unknown;
-  removeListener: (listener: Listener) => unknown;
-  sendMessage: (message: unknown) => unknown;
-  close: () => unknown; // Closes listeners and connections to current event bridge
+	onMessage: (listener: Listener) => unknown;
+	removeListener: (listener: Listener) => unknown;
+	sendMessage: (message: unknown) => unknown;
+	close: () => unknown; // Closes listeners and connections to current event bridge
 }
